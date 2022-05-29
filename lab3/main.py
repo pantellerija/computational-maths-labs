53d9e357c026d5b5ac04d979878ab40e282aa504
pantellerija <polina.pavlova19.10@gmail.com>
2022-05-30 01:04:56 +0300

    fixed methods

--- lab3/main.py
+++ lab3/main.py
@@ -3,8 +3,11 @@ import math
 from lab3 import methods
 
 func_list = [
+    # 2.33 от 1 до 2
     lambda x: x ** 2,
-    lambda x: x ** 3,
+    # 0.786 от 0 до 1
+    lambda x: 1 / (1 + x ** 2),
+    # 4.250 от 0 до 1.5
     lambda x: 2 * x + 1 / math.sqrt(x + 1 / 16)
 ]
 
@@ -17,7 +20,7 @@ if __name__ == '__main__':
     a = 0
     b = 1.5
     eps = 0.0001
-    n = 0
-    ans, num = methods.trapezoidal_method(func_list[2], a, b, eps)
-    print(f'Значение интеграла: %.3f' % ans)
-    print(f'Число разбиений: %3.f' % num)
+    n = 10
+    ans, num = methods.simpson_method(func_list[2], a, b, n, eps)
+    print(f'Значение интеграла: %.4f' % ans)
+    print(f'Число разбиений: %.d' % num)
--- lab3/methods.py
+++ lab3/methods.py
@@ -6,9 +6,9 @@ def trapezoid_sum(func, a, b, n):
     return sum * h
 
 
-def trapezoidal_method(func, a, b, eps):
+def trapezoidal_method(func, a, b, n, eps):
+    print('---Метод трапеций---')
     k = 2
-    n = 10
     h = 1.0 * (b - a) / n
     sum = trapezoid_sum(func, a, b, n) * h
     M = max(1, abs(sum))
@@ -22,20 +22,53 @@ def trapezoidal_method(func, a, b, eps):
 
 def simpson_sum(func, a, b, n):
     h = 1.0 * (b - a) / n
+    if n % 2 == 1:
+        n += 1
     sum = (func(a) + 4 * func(a + h) + func(b))
-    for i in range(1, round(n / 2)):
+    for i in range(1, round(n/2)):
         sum += 2 * func(a + (2 * i) * h) + 4 * func(a + (2 * i + 1) * h)
     return sum * h / 3
 
 
-def simpson_method(func, a, b, eps=1e-8):
+def simpson_method(func, a, b, n, eps):
+    print('---Метод Симпсона---')
     k = 4
-    n = 4
-    sum = simpson_sum(func, a, b, n)
+    h = 1.0 * (b - a) / n
+    sum = (func(a) + 4 * func(a + h) + func(b))
     M = max(1, abs(sum))
     while M > eps:
         prev_sum = sum
         sum = simpson_sum(func, a, b, n)
-        M = abs(sum - prev_sum) / (2 ** k - 1)
         n *= 2
-    return sum, n / 2
+        M = abs(sum - prev_sum) / (2 ** k - 1)
+    return sum, n/2
+
+
+# additional
+
+def double_trapezoid(func, a, b, n):
+    h = (b-a)/2
+    x = a + h/2
+    sum = 0
+    for i in range(n):
+        sum += func(x + i*h)
+    return sum*h/2
+
+
+def simpson_method_with_trap(func, a, b, n, eps):
+    k = 4
+    tr_prev_sum = trapezoid_sum(func, a, b, n)
+    tr_sum = double_trapezoid(func, a, b, n)
+    ans = (4*tr_sum-tr_prev_sum)/3
+    M = max(1, abs(ans))
+    while M > eps:
+        prev_ans = ans
+        tr_prev_sum = tr_sum
+        tr_sum = double_trapezoid(func, a, b, n)
+        ans = (4 * tr_sum - tr_prev_sum) / 3
+        M = abs(ans - prev_ans)/(2**k-1)
+    return ans, n
+
+
+
+
